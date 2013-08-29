acf3bc1f740afded82f226f3b409d0fa92b2c00d
wagyu298 <wagyu298@gmail.com>
2013-08-30 01:21:05 +0900

    Example project modified and screenshot added.

--- Example/CornerRadiusExample/ViewController.m
+++ Example/CornerRadiusExample/ViewController.m
@@ -42,7 +42,12 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *identifier = @"CellIdentifier";
+    NSString *identifier;
+    if (indexPath.section == 0) {
+        identifier = @"FirstSectionCellIdentifier";
+    } else {
+        identifier = @"SecondSectionCellIdentifier";
+    }
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
@@ -50,11 +55,37 @@
     
     // configureCornerRadiusCell methods add some functions to control corner radius
     [tableView configureCornerRadiusCell:cell];
-    cell.cornerRadius = 3.0f;
+    if (indexPath.section == 0) {
+        cell.cornerRadius = 3.0f;
+    } else {
+        cell.cornerRadius = 5.0f;
+    }
     
     cell.selectionStyle = UITableViewCellSelectionStyleBlue;
     cell.textLabel.text = [NSString stringWithFormat:@"section=%d row=%d", indexPath.section, indexPath.row];
     return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return @"3 rows";
+    } else if (section == 1) {
+        return @"Single row";
+    } else {
+        return nil;
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return @"First section configured with\ncorner radius 3px.";
+    } else if (section == 1) {
+        return @"Second section configured with\ncorner radius 5px.";
+    } else {
+        return nil;
+    }
 }
 
 @end
